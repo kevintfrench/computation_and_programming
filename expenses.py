289f3cc73eee7ee1bdf183a91c44017407a5d970
[Kevin <[kfrench@gsinet.net]>
2023-11-25 15:27:13 -0500

    add version 2

--- expenses.py
+++ expenses.py
@@ -1,4 +1,14 @@
-expenses = [10.50, 8, 5, 15, 20, 5, 3]
-total = sum(expenses)
+#Two versions
 
-print("You spent $", total, " on lunch this week.", sep='')
+# first version best for expenses when all are known up front
+# expenses = [10.50, 8, 5, 15, 20, 5, 3]
+# total = sum(expenses)
+# print("You spent $", total, " on lunch this week.", sep='')
+
+#second version for daily entry
+total = 0
+expenses = []
+for i in range(7):
+    expenses.append(float(input("Enter an expense:")))
+total = sum(expenses)
+print("You spent $", total, sep='')
