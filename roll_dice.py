3e2ef6d1c5ea9b9ea1658728b66f2befcebc7ad6
[Kevin <[kfrench@gsinet.net]>
2023-11-25 14:19:28 -0500

    update file

--- roll_dice.py
+++ roll_dice.py
@@ -3,3 +3,5 @@ roll = random.randint(1,6)
 guess = int(input('Guess the dice roll:\n'))
 if guess == roll:
     print("Correct! They rolled a " + str(roll))
+else:
+    print("Wrong! They rolled a " + str(roll))
