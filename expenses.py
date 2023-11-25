a4216ac8c09be801d3c612b2e6fd985c673c3acb
[Kevin <[kfrench@gsinet.net]>
2023-11-25 16:20:27 -0500

    add third version

--- expenses.py
+++ expenses.py
@@ -6,9 +6,21 @@
 # print("You spent $", total, " on lunch this week.", sep='')
 
 #second version for daily entry
+# total = 0
+# expenses = []
+# for i in range(7):
+#     expenses.append(float(input("Enter an expense:")))
+# total = sum(expenses)
+# print("You spent $", total, sep='')
+
+# third version allows user to add range
 total = 0
 expenses = []
-for i in range(7):
+num_expenses = int(input("Enter # of expenses:"))
+for i in range(num_expenses):
     expenses.append(float(input("Enter an expense:")))
+    
 total = sum(expenses)
 print("You spent $", total, sep='')
+    
+    
